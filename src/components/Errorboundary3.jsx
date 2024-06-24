6bf887a92c80fdca6cb55c379ab41a04d08d0f3f
Rume Aluya Joseph <rumealuya3@gmail.com>
2024-06-24 21:14:58 +0100

    Made update to sitemaps

--- src/components/Errorboundary3.jsx
+++ src/components/Errorboundary3.jsx
@@ -1,8 +1,5 @@
 import { Component } from 'react'
 import { Navbar } from './Navbar'
-import Footer from './Footer'
-import Features from './Features'
-
 class ErrorBoundary3 extends Component {
 
   constructor(props) {
@@ -59,8 +56,6 @@ class ErrorBoundary3 extends Component {
               <path d="M1 123.043C67.2858 167.865 259.022 257.325 549.762 188.784C764.181 125.427 967.75 112.601 1200.42 169.707C1347.76 205.869 1901.91 374.562 2201 1" strokeWidth={'3'}></path>
             </svg>
           </div>
-          <Features />
-          <Footer />
         </>
 
 
